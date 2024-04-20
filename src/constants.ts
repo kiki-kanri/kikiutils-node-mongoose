07a07bcfcae1a83a9c96d68c42ff3287c8f7120d
kiki-kanri <a470666@gmail.com>
2024-04-20 23:20:43 +0800

    feat: add `defaultMongooseConnection` constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+import { createConnection } from 'mongoose';
+
 import { createCommonMongooseSchemas } from './utils';
 
 export const commonMongooseSchemas = createCommonMongooseSchemas();
+export const defaultMongooseConnection = createConnection(process.env.MONGODB_URI || '');
