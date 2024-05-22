a72e68293659a6abfb05250c265711b7999ff7c7
kiki-kanri <a470666@gmail.com>
2024-05-22 08:31:39 +0800

    feat: extend `BaseMongoosePaginateModel` to include `AggregatePaginateModel`

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import type { HydratedDocument, PaginateModel, QueryWithHelpers, Types } from 'mongoose';
+import type { AggregatePaginateModel, HydratedDocument, PaginateModel, QueryWithHelpers, Types } from 'mongoose';
 
 // @ts-ignore
 export interface BaseModelStatics<RawDocType, InstanceMethodsAndOverrides = {}, QueryHelpers = {}> {}
@@ -8,7 +8,10 @@ declare global {
 		(CreatedAtField extends true ? { createdAt: Date } : {}) &
 		(UpdatedAtField extends true ? { updatedAt: Date } : {});
 
-	type BaseMongoosePaginateModel<RawDocType, InstanceMethodsAndOverrides = {}, QueryHelpers = {}> = PaginateModel<RawDocType, QueryHelpers, InstanceMethodsAndOverrides> & BaseModelStatics<RawDocType, InstanceMethodsAndOverrides, QueryHelpers>;
+	type BaseMongoosePaginateModel<RawDocType, InstanceMethodsAndOverrides = {}, QueryHelpers = {}> = AggregatePaginateModel<RawDocType, QueryHelpers, InstanceMethodsAndOverrides> &
+		PaginateModel<RawDocType, QueryHelpers, InstanceMethodsAndOverrides> &
+		BaseModelStatics<RawDocType, InstanceMethodsAndOverrides, QueryHelpers>;
+
 	type MongooseDocumentOrObjectId<D> = D | string | Types.ObjectId;
 	type MongooseFindOneReturnType<RawDocType, DocType, QueryHelpers = {}> = QueryWithHelpers<DocType | null, DocType, QueryHelpers, RawDocType, 'findOne'>;
 	type MongooseHydratedDocument<DocType, InstanceMethodsAndOverrides = {}, QueryHelpers = {}> = HydratedDocument<DocType, InstanceMethodsAndOverrides, QueryHelpers>;
