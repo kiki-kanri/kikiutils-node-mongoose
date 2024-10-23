d2c455da798daabbf8b1c0647dfeaa7c5601f8f5
kiki-kanri <a470666@gmail.com>
2024-10-24 02:14:41 +0800

    test: add unit tests for base schema-builder

---
+++ tests/schema-builders/base.test.ts
@@ -0,0 +1,68 @@
+import { Schema } from 'mongoose';
+
+import { createBaseSchemaBuilderFactory } from '../../src/schema-builders/base';
+
+describe('createBaseSchemaBuilderFactory', () => {
+	it('should create a schema with correct type for various constructors', () => {
+		expect(createBaseSchemaBuilderFactory(Boolean)().nonRequired).toEqual({ type: Boolean });
+		expect(createBaseSchemaBuilderFactory(Date)().nonRequired).toEqual({ type: Date });
+		expect(createBaseSchemaBuilderFactory(Number)().nonRequired).toEqual({ type: Number });
+		expect(createBaseSchemaBuilderFactory(Schema.Types.ObjectId)().nonRequired).toEqual({ type: Schema.Types.ObjectId });
+		expect(createBaseSchemaBuilderFactory(String)().nonRequired).toEqual({ type: String });
+	});
+
+	it('should set any additional key in the schema to true', () => {
+		expect(createBaseSchemaBuilderFactory(String)().anOtherKey.anyOtherKey.nonRequired).toEqual({
+			anOtherKey: true,
+			anyOtherKey: true,
+			type: String
+		});
+	});
+
+	it('should set default attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(Number)().default(1).nonRequired).toEqual({ default: 1, type: Number });
+	});
+
+	it('should set default attribute in the schema using a function', () => {
+		function newDateFunction() {
+			return new Date();
+		}
+
+		expect(createBaseSchemaBuilderFactory(Date)().default(newDateFunction).nonRequired).toEqual({ default: newDateFunction, type: Date });
+	});
+
+	it('should set enum attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(Number)().enum([1, 2]).nonRequired).toEqual({ enum: [1, 2], type: Number });
+	});
+
+	it('should set max attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(Number)().max(1024).nonRequired).toEqual({ max: 1024, type: Number });
+	});
+
+	it('should set maxlength attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(String)().maxlength(1024).nonRequired).toEqual({ maxlength: 1024, type: String });
+	});
+
+	it('should set min attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(Number)().min([0, 'min']).nonRequired).toEqual({ min: [0, 'min'], type: Number });
+	});
+
+	it('should set minlength attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(String)().minlength([0, 'minlength']).nonRequired).toEqual({ minlength: [0, 'minlength'], type: String });
+	});
+
+	it('should set required attribute in the schema', () => {
+		expect(createBaseSchemaBuilderFactory(Boolean)().required).toEqual({ required: true, type: Boolean });
+	});
+
+	it('should throw an error when a duplicate schema attribute is set', () => {
+		const schemaBuilder = createBaseSchemaBuilderFactory(Boolean)();
+		expect(() => schemaBuilder.default(false).default(true)).toThrow('Duplicate schema attribute: default');
+	});
+
+	it('should throw an error when using a symbol as a schema attribute', () => {
+		const schemaBuilder = createBaseSchemaBuilderFactory(Boolean)();
+		// @ts-expect-error
+		expect(() => schemaBuilder[Symbol()]).toThrow('Cannot use symbol as a schema attribute');
+	});
+});
