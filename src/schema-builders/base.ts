6a90caaa60f093f3c143143d0e717c0ef1d7e88a
kiki-kanri <a470666@gmail.com>
2024-10-23 10:23:14 +0800

    feat: add createSchemaBuilder function

---
+++ src/schema-builders/base.ts
@@ -0,0 +1,26 @@
+export const createSchemaBuilder = <Builder>(type: NumberConstructor | StringConstructor) => {
+	return () => {
+		const schema: Record<string, any> = { type };
+		return new Proxy(Object.freeze({}), {
+			get(_, key, receiver) {
+				if (typeof key === 'symbol') throw new Error('Cannot use symbol as a schema attribute');
+				if (key in schema) throw new Error(`Duplicate schema attribute: ${key}`);
+				if (isFunctionKeys.has(key)) return (value: any) => ((schema[key] = value), receiver);
+				if (key === 'length') return (value: any) => ((schema['maxlength'] = schema['minlength'] = value), receiver);
+				if (key === 'nonRequired') return schema;
+				if (key === 'required') return { ...schema, required: true };
+				schema[key] = true;
+				return receiver;
+			}
+		}) as Builder;
+	};
+};
+
+const isFunctionKeys = new Set([
+	'default',
+	'enum',
+	'max',
+	'maxlength',
+	'min',
+	'minlength'
+]);
