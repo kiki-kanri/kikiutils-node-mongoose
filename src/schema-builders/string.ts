2c822ba8429d4be8f920348744515f10bd5b046d
kiki-kanri <a470666@gmail.com>
2024-10-23 15:02:31 +0800

    refactor: use constants to define default validation messages for stringSchemaBuilder's ipv4 and ipv6 methods

--- src/schema-builders/string.ts
+++ src/schema-builders/string.ts
@@ -23,8 +23,8 @@ export interface StringSchemaBuilder<Props extends { type: StringSchemaDefinitio
 		value: T
 	) => ExtendStringSchemaBuilder<{ [key in keyof (Props & { enum: T })]: (Props & { enum: T })[key] }, ExtraOmitFields>;
 
-	ipv4: <T extends string = '`{VALUE}` is not a valid IPv4 address for path `{PATH}`.'>(message?: T) => ExtendStringSchemaBuilder<{ [key in keyof (Props & IPSchema<T>)]: (Props & IPSchema<T>)[key] }, ExtraOmitFields | 'ipv4' | 'ipv6'>;
-	ipv6: <T extends string = '`{VALUE}` is not a valid IPv6 address for path `{PATH}`.'>(message?: T) => ExtendStringSchemaBuilder<{ [key in keyof (Props & IPSchema<T>)]: (Props & IPSchema<T>)[key] }, ExtraOmitFields | 'ipv4' | 'ipv6'>;
+	ipv4: <T extends string = typeof defaultIPv4ValidateMessage>(message?: T) => ExtendStringSchemaBuilder<{ [key in keyof (Props & IPSchema<T>)]: (Props & IPSchema<T>)[key] }, ExtraOmitFields | 'ipv4' | 'ipv6'>;
+	ipv6: <T extends string = typeof defaultIPv6ValidateMessage>(message?: T) => ExtendStringSchemaBuilder<{ [key in keyof (Props & IPSchema<T>)]: (Props & IPSchema<T>)[key] }, ExtraOmitFields | 'ipv4' | 'ipv6'>;
 	length: <T extends L | MaybeReadonly<[L, S]>, L extends number, S extends string>(value: T) => ExtendStringSchemaBuilder<{ [key in keyof (Props & { maxlength: T; minlength: T })]: (Props & { maxlength: T; minlength: T })[key] }, ExtraOmitFields>;
 	lowercase: ExtendStringSchemaBuilder<{ [key in keyof (Props & { lowercase: true })]: (Props & { lowercase: true })[key] }, ExtraOmitFields>;
 	maxlength: <T extends L | MaybeReadonly<[L, S]>, L extends number, S extends string>(value: T) => ExtendStringSchemaBuilder<{ [key in keyof (Props & { maxlength: T })]: (Props & { maxlength: T })[key] }, ExtraOmitFields>;
@@ -40,13 +40,15 @@ export interface StringSchemaBuilder<Props extends { type: StringSchemaDefinitio
 }
 
 const baseBuilderFactory = createBaseSchemaBuilderFactory(String);
+const defaultIPv4ValidateMessage = '`{VALUE}` is not a valid IPv4 address for path `{PATH}`.';
+const defaultIPv6ValidateMessage = '`{VALUE}` is not a valid IPv6 address for path `{PATH}`.';
 export const stringSchemaBuilder = () => {
 	const schema: Record<string, any> = {};
 	const baseBuilder = baseBuilderFactory(schema);
 	return new Proxy(baseBuilder, {
 		get(target, key, receiver) {
 			if (key === 'ipv4') {
-				return (message: string = '`{VALUE}` is not a valid IPv4 address for path `{PATH}`.') => {
+				return (message: string = defaultIPv4ValidateMessage) => {
 					schema.trim = true;
 					schema.validate = { message, validator: (value: string) => net.isIPv4(value) };
 					return receiver;
@@ -54,7 +56,7 @@ export const stringSchemaBuilder = () => {
 			}
 
 			if (key === 'ipv6') {
-				return (message: string = '`{VALUE}` is not a valid IPv6 address for path `{PATH}`.') => {
+				return (message: string = defaultIPv6ValidateMessage) => {
 					schema.trim = true;
 					schema.validate = { message, validator: (value: string) => net.isIPv6(value) };
 					return receiver;
