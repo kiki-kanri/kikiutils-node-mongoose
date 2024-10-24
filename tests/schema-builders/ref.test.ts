7c83695fbd69bdea8f7c4e29ef139ea44d087fb0
kiki-kanri <a470666@gmail.com>
2024-10-24 21:30:48 +0800

    test: add unit tests for objectId and number schema builders

---
+++ tests/schema-builders/ref.test.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+import { refSchemaBuilder } from '../../src/schema-builders/ref';
+
+describe('refSchemaBuilder', () => {
+	it('should create a schema with the correct ref for a function returning a model', () => {
+		const schema = new Schema({ name: { type: String } });
+		const getModelFunction = () => model('Model', schema);
+		expect(refSchemaBuilder(getModelFunction).nonRequired).toEqual({ ref: getModelFunction, type: Schema.Types.ObjectId });
+	});
+
+	it('should create a schema with the correct ref for a function returning a model name', () => {
+		const getModelNameFunction = () => 'Model';
+		expect(refSchemaBuilder(getModelNameFunction).nonRequired).toEqual({ ref: getModelNameFunction, type: Schema.Types.ObjectId });
+	});
+
+	it('should create a schema with the correct ref for a model reference', () => {
+		const schema = new Schema({ name: { type: String } });
+		const Model = model('Model', schema);
+		expect(refSchemaBuilder(Model).nonRequired).toEqual({ ref: Model, type: Schema.Types.ObjectId });
+	});
+
+	it('should create a schema with the correct ref for a string reference', () => {
+		expect(refSchemaBuilder('Admin').nonRequired).toEqual({ ref: 'Admin', type: Schema.Types.ObjectId });
+	});
+});
