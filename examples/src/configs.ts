3744a682047dceb50e06c4e029d9ac1af467bc9d
kiki-kanri <a470666@gmail.com>
2024-10-26 23:28:37 +0800

    chore: remove all files in examples folder

--- examples/src/configs.ts
+++
@@ -1,4 +0,0 @@
-import { config } from 'dotenv';
-
-config();
-process.env.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kikiutils-mongoose-example?directConnection=true';
--- examples/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export {} from '@kikiutils/mongoose/types';
-
-declare global {
-	type Nullable<T> = T | null;
-}
